fa100f0bb01a394969610083b2293a969c77e243
UmerJidda <umerjidda5@gmail.com>
2024-06-25 02:30:33 +0530

    Password Generator and Manager

--- Password_Generator&Manager.py
+++ Password_Generator&Manager.py
@@ -1,6 +1,7 @@
 import random
 import cv2
 
+
 list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', "'", '"', ',', '<', '.', '>', '/', '?', '|', '\\', '`', '~']
 password = ''
 print('Welcome to your password generator and manager')
@@ -28,7 +29,7 @@ elif y == 3:
 else:
     print('Invalid input')
 
-
+#Password Generator & Manager
 
 
 
